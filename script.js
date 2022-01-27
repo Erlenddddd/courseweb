cc300a15a12d6a6ceb45b6e96628e6a8f68afe25
Erlend <80337343+Erlenddddd@users.noreply.github.com>
2022-01-27 17:06:47 +0100

    save

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ var el = document.getElementById('str');
 str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
 var running = setTimeout(animate, 90);
 })();
+
+$("#str").html(function(){
+    var text= $(this).text().trim().split(" ");
+    var last = text.pop();
+    return text.join(" ") + (text.length > 0 ? " <span style='color: red;'>" + last + "</span>" : last);
+  });
