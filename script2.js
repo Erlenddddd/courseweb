fda3367e02c4bb33bec75ae96728302186dd96ec
Erlend <80337343+Erlenddddd@users.noreply.github.com>
2022-01-27 15:52:41 +0100

    save

--- script2.js
+++ script2.js
@@ -4,13 +4,13 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='let
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml3 .letter',
+    targets: '.maintext .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
     duration: 2250,
     delay: (el, i) => 150 * (i+1)
   }).add({
-    targets: '.ml3',
+    targets: '.maintext',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
